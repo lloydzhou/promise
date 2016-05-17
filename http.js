3e7fff0f214731cbdc3fb9ce0bc5100fd7c3b25c
lloydzhou <lloydzhou@qq.com>
2016-05-17 23:32:50 +0800

    update ajax function return promise object

--- http.js
+++ http.js
@@ -1,43 +1,45 @@
 ;(function(promise){
     promise.http = function(url, method, data){
-        var core, p, ajax = function(method, uri, args, playload){
-            return p = promise.promise(function(){
+        var core, p, Promise = promise.promise, lower = String.prototype.toLowerCase,
+          ajax = function(method, uri, args){
+            return p = new Promise(function(playload){
                 var client = new XMLHttpRequest(), a = '?', b = '&', e = encodeURIComponent;
-                if (args) {
+                if (typeof args == "object") {
                     for (var key in args) {
                         if (args.hasOwnProperty(key)) {
                             playload += b + e(key) + '=' + e(args[key]);
                         }
                     }
-                }
+                } else playload = args
                 if(!(method === 'POST' || method === 'PUT')){
-                    uri += uri.indexOf(a) > -1 ? b : a + (playload || '')
+                    uri += (uri.indexOf(a) > -1 ? b : a) + (playload || '')
                     playload = null
                 }
                 client.onload = function () {
-                  if (this.status >= 200 && this.status < 300) {
-                    // Performs the function "resolve" when this.status is equal to 2xx
-                    p.resolve(this.response);
+                  if (client.status >= 200 && client.status < 300) {
+                      // Performs the function "resolve" when this.status is equal to 2xx
+                      p.resolve(client.response);
                   } else {
-                    // Performs the function "reject" when this.status is different than 2xx
-                    p.reject(this.statusText);
+                      // Performs the function "reject" when this.status is different than 2xx
+                      p.reject(client.statusText);
                   }
                 };
                 client.onerror = function () {
-                  p.reject(this.statusText);
+                    p.reject(client.statusText);
                 };
                 client.open(method, uri);
                 client.send(playload);
                 return client;
             })
         }
-        core = {
-            get: function(args){return ajax('GET', url, args)},
-            post: function(args){return ajax('POST', url, args)},
-            put: function(args){return ajax('PUT', url, args)},
-            'delete': function(args){return ajax('DELETE', url, args)}
-        }
-        return method ? core[method.toLowerCase()](data) : core
+        core = new Promise(['HEAD', 'GET', 'POST', 'PUT', 'DELETE'].reduce(function(o, v){
+            o[lower.call(v)] = function(args){ return ajax(v, url, args) }
+            return o
+            //return Object.defineProperty(o, lower.call(v), {
+            //    value: function(args){ return ajax(v, url, args) },
+            //})
+        }, {}))
+        return method ? core[lower.call(method)](data) : core
     }
 })(window.promise = window.promise || {})
 
