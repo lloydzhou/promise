c06e04dd46c754dd5a466a18df0036cd1decd777
lloydzhou <lloydzhou@qq.com>
2016-05-18 10:41:07 +0800

    update ajax function, using resolver and reject in construct function.

--- http.js
+++ http.js
@@ -1,9 +1,9 @@
 ;(function(promise){
     promise.http = function(url, method, data){
-        var core, p, Promise = promise.promise, lower = String.prototype.toLowerCase,
+        var core, Promise = promise.promise, lower = String.prototype.toLowerCase,
           ajax = function(method, uri, args){
-            return p = new Promise(function(playload){
-                var client = new XMLHttpRequest(), a = '?', b = '&', e = encodeURIComponent;
+            return new Promise(function(resolver, reject){
+                var client = new XMLHttpRequest(), a = '?', b = '&', e = encodeURIComponent, playload = '';
                 if (typeof args == "object") {
                     for (var key in args) {
                         if (args.hasOwnProperty(key)) {
@@ -11,21 +11,21 @@
                         }
                     }
                 } else playload = args
-                if(!(method === 'POST' || method === 'PUT')){
-                    uri += (uri.indexOf(a) > -1 ? b : a) + (playload || '')
+                if(method != 'POST' && method != 'PUT'){
+                    uri += (uri.indexOf(a) > -1 ? b : a) + playload.substr(1)
                     playload = null
                 }
                 client.onload = function () {
                   if (client.status >= 200 && client.status < 300) {
                       // Performs the function "resolve" when this.status is equal to 2xx
-                      p.resolve(client.response);
+                      resolver(client.response);
                   } else {
                       // Performs the function "reject" when this.status is different than 2xx
-                      p.reject(client.statusText);
+                      reject(client.statusText);
                   }
                 };
                 client.onerror = function () {
-                    p.reject(client.statusText);
+                    reject(client.statusText);
                 };
                 client.open(method, uri);
                 client.send(playload);
