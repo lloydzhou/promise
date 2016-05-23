ac6d44cba5d5ae99efdbb69e065e79f09da64dcc
lloydzhou <lloydzhou@qq.com>
2016-05-23 14:37:31 +0800

    add header to ajax function

--- http.js
+++ http.js
@@ -1,16 +1,22 @@
 ;(function(promise){
-    promise.http = function(url, method, data){
+    promise.http = function(url, method, data, header){
         var Promise = promise.promise,
-          ajax = function(method, uri, args){
+          map = function(args, cb, cb2){
+            if (typeof args == "object") {
+                for (var key in args) {
+                    if (args.hasOwnProperty(key))
+                        cb(key, args[key])
+                }
+            }else cb2 && cb2()
+          },
+          ajax = function(method, uri, args, header){
             return new Promise(function(resolver, reject){
-                var client = new XMLHttpRequest(), a = '?', b = '&', e = encodeURIComponent, playload = '';
-                if (typeof args == "object") {
-                    for (var key in args) {
-                        if (args.hasOwnProperty(key)) {
-                            playload += b + e(key) + '=' + e(args[key]);
-                        }
-                    }
-                } else playload = args
+                var client = new XMLHttpRequest(), a = '?', b = '&'
+                  , e = encodeURIComponent, playload = ''
+                  , c = 'Content-Type', cv = 'application/x-www-form-urlencoded';
+                map(args, function(k, v){
+                    playload += b + e(k) + '=' + e(v)
+                }, function(){ playload = args })
                 if(method != 'POST' && method != 'PUT'){
                     uri += (uri.indexOf(a) > -1 ? b : a) + playload.substr(1)
                     playload = null
@@ -28,15 +34,20 @@
                     reject(client.statusText);
                 };
                 client.open(method, uri);
+                header = header || {}
+                header[c] = header[c] || cv
+                map(header, function(k, v){
+                    client.setRequestHeader(k, v)
+                })
                 client.send(playload);
                 return client;
             })
         },
         core = new Promise(['HEAD', 'GET', 'POST', 'PUT', 'DELETE'].reduce(function(o, v){
-            o[v.toLowerCase()] = function(args){ return ajax(v, url, args) }
+            o[v.toLowerCase()] = function(args, header){ return ajax(v, url, args, header) }
             return o
         }, {}))
-        return method ? core[method.toLowerCase()](data) : core
+        return method ? core[method.toLowerCase()](data, header) : core
     }
 })(window.promise = window.promise || {})
 
